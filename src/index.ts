4d5968c095b601f146f6b431d4f069d9c1dab401
fazzal-dev <fazzal.dev@gmail.com>
2024-09-19 01:43:32 +0500

    added user routes

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,40 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+interface UserData {
+  userId: string;
+  data: any;
+}
+
+const users: User[] = [];
+const userData: UserData[] = [];
+
+app.post("/user", (req, res) => {
+  const { name, email } = req.body;
+  const id = Date.now().toString();
+  const newUser: User = { id, name, email };
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+app.post("/user/:id/data", (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+  const newUserData: UserData = { userId: id, data };
+  userData.push(newUserData);
+  res.status(201).json(newUserData);
+});
+
+app.get("/user/:id/data", (req, res) => {
+  const { id } = req.params;
+  const userDataItems = userData.filter((item) => item.userId === id);
+  res.json(userDataItems);
 });
 
 app.listen(port, () => {
